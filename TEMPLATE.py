c8a7bf065ea1194ef81593aab2a87d4698e11d7a
Mohamad Tawakol <tawakool@gmail.com>
2013-11-25 12:18:36 -0400

    added styling and template file
    use the TEMPLATE.py file as a starting point because the structure is
    correct for the styling to work for everyone

---
+++ TEMPLATE.py
@@ -0,0 +1,35 @@
+#!/local/bin/python
+
+import os
+import cgi
+import cgitb; cgitb.enable()
+import sqlite3
+import sys
+
+url_args = cgi.FieldStorage()
+args = {x: url_args.getvalue(x) for x in url_args.keys()}
+
+print "Content-type: text/html\n"
+    
+print "<html><head><link href='Media/style.css' rel = 'stylesheet' type = 'text/css' media='all'/></head><body>"
+
+
+print "<div id = 'main'>"
+print "<div id = 'container'>"
+
+#put all html code inside mainbody
+print "<div id = 'mainbody'>" 
+
+print "<h1>This is just a template</h1>"
+
+#this is a section break (it has an image to make it look cool):
+print "<hr />"
+
+print "<br />"
+
+print "</div>"
+#put all html code above this hashtag (unless you don't want it in the main body)
+print "</div>"
+print "</div>"
+
+print "</body></html>"
