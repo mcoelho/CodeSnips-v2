9cca7febf8505dafa00a3a216088734e00a5be9f
Mohamad Tawakol <tawakool@gmail.com>
2013-11-26 19:33:43 -0400

    added the push file to push new snippets

--- snippets/push.py
+++ snippets/push.py
@@ -5,6 +5,10 @@ import cgi
 import cgitb; cgitb.enable()
 import sqlite3
 import sys
+pkg = "~/public_html/oop/codesnips"
+sys.path.append(os.path.dirname(os.path.expanduser(pkg)))
+from codesnips.data import dbCommands
+from codesnips.models import Snippet
 
 form = cgi.FieldStorage();
 
@@ -13,5 +17,5 @@ snippetDesc = form.getvalue('snippetDesc')
 snippetLang = form.getvalue('snippetLang')
 snippetCode = form.getvalue('snippetCode')
 
-cmd = dbCommands.AddToDatabaseCommand("Snippet", "language, name, description, code" )
-rows = cmd.execute()
+cmd = dbCommands.AddToDatabaseCommand("Snippet", "user, language, name, description, code", "'1','" + snippetLang + "','" + snippetTitle + "','" + snippetDesc + "','" + snippetCode + "'")
+cmd.execute() 
