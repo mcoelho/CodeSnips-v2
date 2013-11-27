da384950c7e98d1ac94ddc57bb1e575c507df3d7
Cameron <cam_sieffert@hotmail.com>
2013-11-26 22:43:33 -0400

    Edited the Language function
    Added the createVersion factory method that is overridden by the classes
    extending Language

--- codesnips/models/Language.py
+++ codesnips/models/Language.py
@@ -9,4 +9,7 @@ class Language(object):
     
     def __init__(self):
         Language.count += 1
+        self.version = self.createVersion()
         
+    def createVersion(self):
+        return 'No version
