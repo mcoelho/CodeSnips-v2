61f828b77c4120867738be3c6452ba5b64626789
Cameron <cam_sieffert@hotmail.com>
2013-11-30 16:44:57 -0400

    Adding view.py to languages folder

---
+++ codesnips/models/languages/view.py
@@ -0,0 +1,37 @@
+#!/local/bin/python
+
+import cgi
+import cgitb; cgitb.enable()
+import AbstractLanguageFactory
+
+def view(self, languageName, versionNumber):
+
+    factory = AbstractLanguageFactory()
+    language = factory.createLanguage(languageName, versionNumber)
+    
+    print "Language name: " + language.name
+    print "Creator: " + language.creator
+    print "Year introduced: " + language.yearIntroduced
+    print "Operating system: " + language.operatingSystem
+    print "Version: " + language.versionNumber
+    print "Deprecated Functions: " 
+    print  language.version.depFunctions
+    print "New functions: "
+    print  language.version.newFunctions
+    print  "All functions:"
+    print  language.version.allFunctions
+
+url_args = cgi.FieldStorage()
+args = {x: url_args.getvalue(x) for x in url_args.keys()}
+if 'languageName' in args:
+    if 'versionNumber' in args:
+
+        print "Content-type: text/html\n"
+    
+        print "<html><head></head><body>"
+    
+        print "<h1>View Test</h1>"
+
+        view(args['languageName'], args['versionNumber'])
+
+        print "</body></html>"
