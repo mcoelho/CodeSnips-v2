a4a38518ccb49b078c4a0f24428356845ebacdd6
Mohamad Tawakol <tawakool@gmail.com>
2013-12-03 21:18:48 -0400

    updated some stuff

--- snippets/view.py
+++ snippets/view.py
@@ -10,7 +10,10 @@ pkg = "~/public_html/oop/codesnips"
 sys.path.append(os.path.dirname(os.path.expanduser(pkg)))
 from codesnips.data import dbCommands
 
-form = cgi.FieldStorage()
+url_args = cgi.FieldStorage()
+args = {x: url_args.getvalue(x) for x in url_args.keys()}
+
+uid = args['uid']
 
 cmd = dbCommands.ReadFromDatabaseCommand("Snippet")
 rows = cmd.execute()
@@ -31,9 +34,9 @@ print "<a href=http://web.cs.dal.ca/~coelho/oop/index1.py><img src='../Media/log
 
 print "<div id = 'navblock'>"
 print "<ul>"
-print "<li><a href=http://web.cs.dal.ca/~coelho/oop/index.py>Home</a></li>"
-print "<li><a style='color:black' href=http://web.cs.dal.ca/~coelho/oop/snippets/view.py>View Snippets</a></li>"
-print "<li><a href=http://web.cs.dal.ca/~coelho/oop/snippets/create.py>Create Snippet</a></li>"
+print "<li><a href=http://web.cs.dal.ca/~coelho/oop/index.py?uid=" + uid + ">Home</a></li>"
+print "<li><a style='color:black' href=http://web.cs.dal.ca/~coelho/oop/snippets/view.py?uid=" + uid + ">View Snippets</a></li>"
+print "<li><a href=http://web.cs.dal.ca/~coelho/oop/snippets/create.py?uid=" + uid + ">Create Snippet</a></li>"
 print "<li><a href=#>View Langauges</a></li>"
 print "<li><a href=http://web.cs.dal.ca/~coelho/oop/logout.py>Log-out</a></li>"
 print "</ul>" 
