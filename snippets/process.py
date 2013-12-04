692a47a8b42dca995e032eb7946a173d4760d419
Yumingz <yzhang@cs.dal.ca>
2013-12-03 21:18:58 -0400

    get process.py back

---
+++ snippets/process.py
@@ -0,0 +1,22 @@
+#!/local/bin/python
+
+import os, sys, inspect, datetime
+import cgi
+import cgitb; cgitb.enable()
+pkg = "~/public_html/oop/codesnips"
+sys.path.append(os.path.dirname(os.path.expanduser(pkg)))
+from codesnips.data import dbCommands
+
+print "Content-type: text/html\n"
+
+form = cgi.FieldStorage()
+snippetTitle = form.getvalue('snippetTitle')
+snippetDesc = form.getvalue('snippetDesc')
+snippetLang = form.getvalue('snippetLang')
+snippetVersion = form.getvalue('snippetVersion')
+snippetCode = form.getvalue('snippetCode')
+
+cmd = dbCommands.AddToDatabaseCommand("Snippet", "userId, language, version, title, description, code, lastChanged", "1, '" + str(snippetLang) + "', '" + str(snippetVersion) + "', '" + str(snippetTitle) + "', '" + str(snippetDesc) + "', '" + str(snippetCode) + "', '" + str(datetime.datetime.now()) +"'")
+cmd.execute()
+
+print '<meta http-equiv="refresh" content="0;url=http://web.cs.dal.ca/~coelho/oop/snippets/view.py" />'
